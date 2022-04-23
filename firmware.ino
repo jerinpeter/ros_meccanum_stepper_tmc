270652664982679a1d1d7c425b90ee2e2b1c6c68
Jerin Peter <34794384+jerinpeter@users.noreply.github.com>
2022-04-23 11:39:23 +0530

    Update firmware.ino
    Test for 2 motors

--- firmware.ino
+++ firmware.ino
@@ -34,19 +34,33 @@ double rpm_motor_br;
 double rpm_motor_bl;
 
 
-#define EN_PIN           A8 // Enable
-#define DIR_PIN          48 // Direction
-#define STEP_PIN         46 // Step
+#define FR_EN_PIN           A8 // Enable
+#define FR_DIR_PIN          48 // Direction
+#define FR_STEP_PIN         46 // Step
 //#define SW_RX            A11 // TMC2208/TMC2224 SoftwareSerial receive pin
 //#define SW_TX            D42 // TMC2208/TMC2224 SoftwareSerial transmit pin
-#define SERIAL_PORT Serial1 // Arduino mega Hw serial pins Tx=18 and Rx=19 
+#define FR_SERIAL_PORT Serial1 // Arduino mega Hw serial pins Tx=18 and Rx=19 
+
+
+#define FL_EN_PIN           A8 // Enable
+#define FL_DIR_PIN          48 // Direction
+#define FL_STEP_PIN         46 // Step
+//#define SW_RX            A11 // TMC2208/TMC2224 SoftwareSerial receive pin
+//#define SW_TX            D42 // TMC2208/TMC2224 SoftwareSerial transmit pin
+#define FL_SERIAL_PORT Serial2 // Arduino mega Hw serial pins Tx=18 and Rx=19
+
+
 #define DRIVER_ADDRESS 0b00 // TMC2209 Driver address according to MS1 and MS2
 #define R_SENSE 0.11f // Match to your driver
 
-TMC2209Stepper driver(&SERIAL_PORT, R_SENSE, DRIVER_ADDRESS);
+TMC2209Stepper FR_driver(&FR_SERIAL_PORT, R_SENSE, DRIVER_ADDRESS);
+TMC2209Stepper FL_driver(&FL_SERIAL_PORT, R_SENSE, DRIVER_ADDRESS);
+
 constexpr uint32_t steps_per_m = 1911; // steps needed for robot to move 1m
 
-AccelStepper stepper = AccelStepper(stepper.DRIVER, STEP_PIN, DIR_PIN);
+AccelStepper FR_stepper = AccelStepper(FR_stepper.DRIVER, FR_STEP_PIN, FR_DIR_PIN);
+AccelStepper FL_stepper = AccelStepper(FL_stepper.DRIVER, FL_STEP_PIN, FL_DIR_PIN);
+
 
 
 void messageCb(const geometry_msgs::Twist& msg)  // cmd_vel callback function definition
@@ -79,14 +93,20 @@ void messageCb(const geometry_msgs::Twist& msg)  // cmd_vel callback function de
 if((rpm_motor_fl) && (rpm_motor_bl) && (rpm_motor_fr) && (rpm_motor_br) > 0)
 {
 
-      // insert all motors forward code here + multiply rpm with 10
+    FR_stepper.setSpeed(rpm_motor_fr*10);
+    FL_stepper.setSpeed(rpm_motor_fl*10);
+    FR_stepper.runSpeed();
+    FL_stepper.runSpeed();
 }
   
   
   else if((rpm_motor_fl) && (rpm_motor_bl) && (rpm_motor_fr) && (rpm_motor_br) < 0)
 {
  
-      // insert all motors backward code here + multiply rpm with 10
+    FR_stepper.setSpeed(rpm_motor_fr*10);
+    FL_stepper.setSpeed(rpm_motor_fl*10);
+    FR_stepper.runSpeed();
+    FL_stepper.runSpeed();
 }
 
 
@@ -100,20 +120,36 @@ void setup() {
      nh.initNode();      // initialzing the node handle object
      nh.subscribe(sub);  // subscribing to cmd vel with sub object
     
-    SERIAL_PORT.begin(115200);      // HW UART drivers
+    FR_SERIAL_PORT.begin(115200);      // HW UART drivers
     //driver.begin();             // Initiate pins and registeries
-    driver.toff(5);                 // Enables driver in software
-    driver.rms_current(900);    // Set stepper current to 600mA. The command is the same as command TMC2130.setCurrent(600, 0.11, 0.5);
-    driver.en_spreadCycle(true);   // Toggle spreadCycle on TMC2208/2209/2224
+    FR_driver.toff(5);                 // Enables driver in software
+    FR_driver.rms_current(900);    // Set stepper current to 600mA. The command is the same as command TMC2130.setCurrent(600, 0.11, 0.5);
+    FR_driver.en_spreadCycle(true);   // Toggle spreadCycle on TMC2208/2209/2224
     //driver.pwm_autoscale(1);
-    driver.intpol(true); //1/256 microstep interpolation
-    driver.microsteps(1); //Microsteps. 1 = full step
+    FR_driver.intpol(true); //1/256 microstep interpolation
+    FR_driver.microsteps(1); //Microsteps. 1 = full step
     
-    stepper.setMaxSpeed(0.5*steps_per_m); //0.5m/s
-    stepper.setAcceleration(0.1*steps_per_m); // 0.1m/s^2
-    stepper.setEnablePin(EN_PIN);
-    stepper.setPinsInverted(false, false, true);
-    stepper.enableOutputs();
+    FR_stepper.setMaxSpeed(0.5*steps_per_m); //0.5m/s
+    FR_stepper.setAcceleration(0.1*steps_per_m); // 0.1m/s^2
+    FR_stepper.setEnablePin(FR_EN_PIN);
+    FR_stepper.setPinsInverted(false, false, true);
+    FR_stepper.enableOutputs();
+
+    FL_SERIAL_PORT.begin(115200);      // HW UART drivers
+    //driver.begin();             // Initiate pins and registeries
+    FL_driver.toff(5);                 // Enables driver in software
+    FL_driver.rms_current(900);    // Set stepper current to 600mA. The command is the same as command TMC2130.setCurrent(600, 0.11, 0.5);
+    FL_driver.en_spreadCycle(true);   // Toggle spreadCycle on TMC2208/2209/2224
+    //driver.pwm_autoscale(1);
+    FL_driver.intpol(true); //1/256 microstep interpolation
+    FL_driver.microsteps(1); //Microsteps. 1 = full step
+    
+    FL_stepper.setMaxSpeed(0.5*steps_per_m); //0.5m/s
+    FL_stepper.setAcceleration(0.1*steps_per_m); // 0.1m/s^2
+    FL_stepper.setEnablePin(FL_EN_PIN);
+    FL_stepper.setPinsInverted(false, false, true);
+    FL_stepper.enableOutputs();
+
     
 }
 
