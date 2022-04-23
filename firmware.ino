fbfba69a529867cb2519c9dc814bd7a72d21d103
Jerin Peter <34794384+jerinpeter@users.noreply.github.com>
2022-04-23 11:37:16 +0530

    Updated with RPM method + test publish wheel vel
    Adopted RPM based method for providing motor steps

--- firmware.ino
+++ firmware.ino
@@ -7,27 +7,9 @@
 #include <AccelStepper.h>
 #define MOTOR_STEPS 200
 
-////Front Right
-//#define STEP1 3
-//#define front_right_DIR 4
-//
-////Front Left
-//#define STEP2 5
-//#define front_left_DIR 6
-//
-////Rear Right
-//#define STEP3 7
-//#define rear_right_DIR 8
-//
-//
-////Rear Left
-//#define STEP4 9
-//#define rear_rightDIR 10
-//
-//A4988 front_right_motor(MOTOR_STEPS, front_right_DIR, STEP1);
-//A4988 front_left_motor(MOTOR_STEPS, front_left_DIR, STEP2);
-//A4988 rear_right_motor(MOTOR_STEPS, rear_right_DIR, STEP3);
-//A4988 rear_left_motor(MOTOR_STEPS, rear_right_DIR, STEP4);
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2, 3); // RX, TX
 
 AccelStepper LeftBackWheel(1, 42, 43);   // (Type:driver, STEP, DIR) - Stepper1
 AccelStepper LeftFrontWheel(1, 40, 41);  // Stepper2
@@ -39,20 +21,34 @@ int wheelSpeed = 1500;
 ros::NodeHandle nh;        // Node handle Object
 geometry_msgs::Twist msg;  // msg variable of data type twist
 
-//std_msgs::Int32 value;  
+std_msgs::Int32 value;  
  
+double rpm_motor_fl;
+double rpm_motor_fr;
+double rpm_motor_br;
+double rpm_motor_bl;
 
-//ros::Publisher motor_value("motor_value", &value);
-
-
+ros::Publisher motor_value("motor_value", &value);
+double circumference_ = 0.314;
+double tangential_vel_;
 double speed_x;
 double speed_y;
 double speed_rot;
 
-double speed_front_left;
-double speed_front_right;
-double speed_back_left;
-double speed_back_right;
+//double speed_front_left;
+//double speed_front_right;
+//double speed_back_left;
+//double speed_back_right;
+
+double linear_vel_x_mins_;
+double linear_vel_y_mins_;
+double angular_vel_z_mins_;
+
+double x_rpm_;
+double y_rpm_;
+double tan_rpm_;
+
+
 
 
 double wheel_radius = 0.05;
@@ -68,44 +64,62 @@ double wheel_geometry = (wheel_seperation_width + wheel_seperation_length) / 2;
 
 void messageCb(const geometry_msgs::Twist& msg)  // cmd_vel callback function definition
  {
-  speed_x = max(min(msg.linear.x, 1.0f), -1.0f);   // limits the linear x value from -1 to 1
-  speed_y = max(min(msg.linear.y, 1.0f), -1.0f);  // limits the angular z value from -1 to 1
-  speed_rot = max(min(msg.angular.z, 1.0f), -1.0f);  // limits the angular z value from -1 to 1
+  speed_x = max(min(msg.linear.x, 1.0f), -1.0f);   
+  speed_y = max(min(msg.linear.y, 1.0f), -1.0f); 
+  speed_rot = max(min(msg.angular.z, 1.0f), -1.0f); 
 
-    front_left = (speed_x - speed_y - speed_rot * wheel_geometry) / wheel_radius;
-    front_right = (speed_x + speed_y + speed_rot * wheel_geometry) / wheel_radius;
-    back_left = (speed_x + speed_y - speed_rot * wheel_geometry) / wheel_radius;
-    back_right = (speed_x - speed_y + speed_rot * wheel_geometry) / wheel_radius;
-    
- if((front_left) && (front_right) && (back_left) && (back_right) == 0)
- {
-stopMoving();
- 
-  }
+  linear_vel_x_mins_ = speed_x * 60;
+  linear_vel_y_mins_ = speed_y * 60;
+  angular_vel_z_mins_ = speed_rot * 60;
+  
+  tangential_vel_ = angular_vel_z_mins_ * 0.37;  // 0.37 -> base width
 
-  else {
-  speed_front_left = mapFloat(fabs(front_left), 0.0, 20.0, 100,3000);
-  speed_front_right = mapFloat(fabs(front_right), 0.0, 20.0, 100,3000);
-  speed_back_right= mapFloat(fabs(back_right), 0.0, 20.0, 100,3000);
-  speed_back_left = mapFloat(fabs(back_left), 0.0, 20.0, 100,3000);
-  }
+   x_rpm_ = linear_vel_x_mins_ / circumference_;
+  y_rpm_ = linear_vel_y_mins_ / circumference_;
+  tan_rpm_ = tangential_vel_ / circumference_;
 
-if((front_left) && (front_right) && (back_left) && (back_right) > 0) 
- 
-  moveForward();
-   
- else if((front_left) && (front_right) && (back_left) && (back_right) > 0)
-  
-  moveBackward();
-  
-  else if(((front_left) && (back_left) > 0) && ((back_left) && (front_left) < 0))
-  
-  rotateRight();
-  
-  else if(((front_left) && (back_left) < 0) && ((back_left) && (front_left) > 0))
-  
-  rotateLeft();
+   rpm_motor_fl = x_rpm_ - y_rpm_ - tan_rpm_;
+  //rear-left motor
+  rpm_motor_bl = x_rpm_ + y_rpm_ - tan_rpm_;
 
+  //front-right motor
+  rpm_motor_fr = x_rpm_ + y_rpm_ + tan_rpm_;
+  //rear-right motor
+  rpm_motor_br = x_rpm_ - y_rpm_ + tan_rpm_;
+
+//    front_left = (speed_x - speed_y - speed_rot * wheel_geometry) / wheel_radius;
+//    front_right = (speed_x + speed_y + speed_rot * wheel_geometry) / wheel_radius;
+//    back_left = (speed_x + speed_y - speed_rot * wheel_geometry) / wheel_radius;
+//    back_right = (speed_x - speed_y + speed_rot * wheel_geometry) / wheel_radius;
+//    
+// if((front_left) && (front_right) && (back_left) && (back_right) == 0)
+// {
+//stopMoving();
+// 
+//  }
+//
+//  else {
+//  speed_front_left = mapFloat(fabs(front_left), 0.0, 20.0, 100,3000);
+//  speed_front_right = mapFloat(fabs(front_right), 0.0, 20.0, 100,3000);
+//  speed_back_right= mapFloat(fabs(back_right), 0.0, 20.0, 100,3000);
+//  speed_back_left = mapFloat(fabs(back_left), 0.0, 20.0, 100,3000);
+//  }
+//
+//if((front_left) && (front_right) && (back_left) && (back_right) > 0) 
+// 
+//  moveForward();
+//   
+// else if((front_left) && (front_right) && (back_left) && (back_right) > 0)
+//  
+//  moveBackward();
+//  
+//  else if(((front_left) && (back_left) > 0) && ((back_left) && (front_left) < 0))
+//  
+//  rotateRight();
+//  
+//  else if(((front_left) && (back_left) < 0) && ((back_left) && (front_left) > 0))
+//  
+//  rotateLeft();
 
   }
 
@@ -124,81 +138,13 @@ void setup()
 
      nh.initNode();      // initialzing the node handle object
      nh.subscribe(sub);  // subscribing to cmd vel with sub object
-//    nh.advertise(motor_value);
+   nh.advertise(motor_value);
 
   }
 
 void loop() {
-//  value.data = back_right;
-//  motor_value.publish(&value);
+  value.data = rpm_motor_fl;
+  motor_value.publish(&value);
   nh.spinOnce();
  
-}
-
-
-void moveForward() {
-  LeftFrontWheel.setSpeed(speed_front_left);
-  LeftBackWheel.setSpeed(speed_back_left);
-  RightFrontWheel.setSpeed(speed_front_right);
-  RightBackWheel.setSpeed(speed_back_right);
-}
-void moveBackward() {
-  LeftFrontWheel.setSpeed(-speed_front_left);
-  LeftBackWheel.setSpeed(-speed_back_left);
-  RightFrontWheel.setSpeed(-speed_front_right);
-  RightBackWheel.setSpeed(-speed_back_right);
-}
-void moveSidewaysRight() {
-  LeftFrontWheel.setSpeed(speed_front_left);
-  LeftBackWheel.setSpeed(-speed_back_left);
-  RightFrontWheel.setSpeed(-speed_front_right);
-  RightBackWheel.setSpeed(speed_back_right);
-}
-void moveSidewaysLeft() {
-  LeftFrontWheel.setSpeed(-speed_front_left);
-  LeftBackWheel.setSpeed(speed_back_left);
-  RightFrontWheel.setSpeed(speed_front_right);
-  RightBackWheel.setSpeed(-speed_back_right);
-}
-void rotateLeft() {
-  LeftFrontWheel.setSpeed(-speed_front_left);
-  LeftBackWheel.setSpeed(-speed_back_left);
-  RightFrontWheel.setSpeed(speed_front_right);
-  RightBackWheel.setSpeed(speed_back_right);
-}
-void rotateRight() {
-  LeftFrontWheel.setSpeed(speed_front_left);
-  LeftBackWheel.setSpeed(speed_back_left);
-  RightFrontWheel.setSpeed(-speed_front_right);
-  RightBackWheel.setSpeed(-speed_back_right);
-}
-void moveRightForward() {
-  LeftFrontWheel.setSpeed(speed_front_left);
-  LeftBackWheel.setSpeed(0);
-  RightFrontWheel.setSpeed(0);
-  RightBackWheel.setSpeed(speed_back_right);
-}
-void moveRightBackward() {
-  LeftFrontWheel.setSpeed(0);
-  LeftBackWheel.setSpeed(-speed_back_left);
-  RightFrontWheel.setSpeed(-speed_front_right);
-  RightBackWheel.setSpeed(0);
-}
-void moveLeftForward() {
-  LeftFrontWheel.setSpeed(0);
-  LeftBackWheel.setSpeed(speed_back_left);
-  RightFrontWheel.setSpeed(speed_front_right);
-  RightBackWheel.setSpeed(0);
-}
-void moveLeftBackward() {
-  LeftFrontWheel.setSpeed(-speed_front_left);
-  LeftBackWheel.setSpeed(0);
-  RightFrontWheel.setSpeed(0);
-  RightBackWheel.setSpeed(-speed_back_right);
-}
-void stopMoving() {
-  LeftFrontWheel.setSpeed(0);
-  LeftBackWheel.setSpeed(0);
-  RightFrontWheel.setSpeed(0);
-  RightBackWheel.setSpeed(0);
 }
